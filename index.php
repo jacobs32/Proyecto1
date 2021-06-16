dc57d0d7d710bc801b740092279600ef3ff96e2b
Jacob Cardenas <201520407e@utea.edu.pe>
2021-06-15 22:27:04 -0500

    Se mejoro la interfaz del Login y los estilos

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <title>Login con PHP - Bootstrap 4</title>
 
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="estiloss.css">
+        <link rel="stylesheet" href="estilos.css">
 
         <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">
 
@@ -20,8 +20,37 @@
         <h3 class="text-center display-4">Login</h3>
 
         <div class="container">
-            <div class="row">
-                <div class="col-md-6">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+
+                <div id="login-box" class="col-md-12 bg-light text-dark">
+                    <form id="formlogin" class="form" action="" method="post">
+
+                        <div class="form-group">
+                            <label for="usuario" class="text-dark">Usuario</label>
+                            <input type="text" name="usuario" id="usuario" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="text-dark">Contraseña</label>
+                            <input type="password" name="password" id="password" class="form-control">
+                        </div>
+
+                        <h1></h1>
+
+                        <div class="form-gropu text-center">
+                            <input type="submit" name="submit" class="btn btn-dark btn-lg btn-block" value="Login"
+                            <div class="form-gropu text-center">
+                            <input type="submit" name="submit" class="btn btn-dark btn-lg btn-block" value="Ir a registro">
+                        </div>
+                            
+                        </div>
+
+                        <h1></h1>
+
+                        
+
+                    </form>
+                </div>
                 </div>
             </div>
         </div>
