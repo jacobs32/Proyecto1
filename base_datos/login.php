9738186ea3697010568655d2be3b57ff4ed00459
Jacob Cardenas <201520407e@utea.edu.pe>
2021-06-16 13:04:06 -0500

    Creacion de base de datos con objeto PDO

--- base_datos/login.php
+++ base_datos/login.php
@@ -1,31 +1,7 @@
 <?php
-session_start();
-
 include_once 'conexion.php';
 $objeto = new Conexion();
 $conexion = $objeto->Conectar();
 
-//recepción de datos enviados mediante POST desde ajax
-$usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : '';
-$password = (isset($_POST['contraseña'])) ? $_POST['contraseña'] : '';
-
-$pass = md5($password); //encripto la clave enviada por el usuario para compararla con la clava encriptada y almacenada en la BD
-
-$consulta = "SELECT * FROM usuarios WHERE usuario='$usuario' AND password='$pass' ";
-$resultado = $conexion->prepare($consulta);
-$resultado->execute();
-
-if($resultado->rowCount() >= 1){
-    $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
-    $_SESSION["s_usuario"] = $usuario;
-}else{
-    $_SESSION["s_usuario"] = null;
-    $data=null;
-}
-
-print json_encode($data);
-$conexion=null;
-
-//usuarios de pruebaen la base de datos
-//usuario:admin pass:12345
-//usuario:demo pass:demo
+print_r($conexion);
+?>
