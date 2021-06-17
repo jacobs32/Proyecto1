1ac5da0f1f044f9067e1e59ec76197fbd74e492a
Jacob Cardenas <201520407e@utea.edu.pe>
2021-06-17 08:35:09 -0500

    Consulta para registrar usuario

--- base_datos/login.php
+++ base_datos/login.php
@@ -1,7 +1,34 @@
 <?php
+session_start();
+
 include_once 'conexion.php';
 $objeto = new Conexion();
 $conexion = $objeto->Conectar();
 
-print_r($conexion);
+
+//recepcion de datos enviados mediante POST desde ajax
+
+$usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : '';
+$usuario = (isset($_POST['password'])) ? $_POST['password'] : '';
+
+//encripto la clave enviada por el usuario para compararla con la clave
+//encriptada y almacenada en la base de datos
+
+$pass = md5(password);
+
+$consulta = "SELECT * FROM registros WHERE usuario='$usuario' AND password='$pass' ";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+
+if($resultado->rowCount() >= 1){
+    $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION["s_usuario"] = $usuario;
+}else{
+    $_SESSION["s_usuario"] = null;
+    $data=null;
+}
+
+print json_encode($data);
+$conexion=null;
+
 ?>
