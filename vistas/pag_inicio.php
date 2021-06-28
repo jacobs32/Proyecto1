ac7d1082593028179a58e0e05efe61bd893e8108
Jacob Cardenas <201520407e@utea.edu.pe>
2021-06-27 21:09:25 -0500

    Se agrego datatable y se agrego interfaz vistas

--- vistas/pag_inicio.php
+++ vistas/pag_inicio.php
@@ -1,51 +1,120 @@
 <?php
-session_start();
+include_once '/bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
 
-if($_SESSION["s_usuario"] === null){
-    header("location: ../index.php");
-}
+$consulta = "SELECT id, tarea, accion, FROM usuarios";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<doctype html>
-    <html>
 
-    <head>
-        <link rel="shortcut icon" href="#" />
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Login con PHP - Bootstrap 4</title>
-
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../estilos.css">
-
-        <link rel="stylesheet" href="../plugins/sweetalert2/sweetalert2.min.css">
-
-    </head>
-
-    <body>
-        <div class="container">
-            <div class="row">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="#" />  
+    <title>Tutorial DataTables</title>
+      
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- CSS personalizado --> 
+    <link rel="stylesheet" href="main.css">  
+      
+      
+    <!--datables CSS básico-->
+    <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
+    <!--datables estilo bootstrap 4 CSS-->  
+    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">       
+  </head>
+    
+  <body> 
+     <header>
+<!--         <h3 class="text-center text-light">Tutorial</h3>-->
+         <h4 class="text-center text-light">ListaTareas con <span class="badge badge-danger">DATATABLES</span></h4> 
+     </header>    
+      
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">            
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
+            </div>    
+        </div>    
+    </div>    
+    <br>  
+    <div class="container">
+        <div class="row">
                 <div class="col-lg-12">
-                    <div class="jumbotron">
-
-                    <h1 class="display-4 text-center">¡ Bienvenido!</h1>
-                    <h2 class="text-center">Usuario: <span class="badge badge=primary">
-                                       <?php echo $_SESSION["s_usuario"];?></span></h2>
-                    <p class="lead text-center">Esta es la pagina de inicio, luego de un LOGIN correcto.</p>
-                    <hr class="my-4">
-                    <a class="btn btn-danger btn-lg" href="../base_datos.php" role="button">Cerrar Sesión</a>
-                        
+                    <div class="table-responsive">        
+                        <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" style="width:100%">
+                        <thead class="text-center">
+                            <tr>
+                                <th>Id</th>
+                                <th>Tarea</th>
+                                <th>Accion</th>                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php                            
+                            foreach($data as $dat) {                                                        
+                            ?>
+                            <tr>
+                                <td><?php echo $dat['id'] ?></td>
+                                <td><?php echo $dat['tarea'] ?></td>
+                                <td><?php echo $dat['accion'] ?></td>  
+                                <td></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>                                
+                        </tbody>        
+                       </table>                    
                     </div>
                 </div>
+        </div>  
+    </div>    
+      
+<!--Modal para CRUD-->
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
             </div>
+        <form id="formPersonas">    
+            <div class="modal-body">
+                <div class="form-group">
+                <label for="tarea" class="col-form-label">Tarea:</label>
+                <input type="text" class="form-control" id="tarea">
+                </div>
+                <div class="form-group">
+                <label for="accion" class="col-form-label">Accion:</label>
+                <input type="text" class="form-control" id="accion">
+                </div>                          
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+            </div>
+        </form>    
         </div>
-
-
-        <script src="../jquery/jquery-3.3.1.min.js"></script>
-        <script src="../bootstrap/js/bootstrap.min.js"></script>
-        <script src="../popper/popper.min.js"></script>
-
-        <script src="../plugins/sweetalert2/sweetalert2.all.min.js"></script> 
-        <script src="../codigo.js"></script>
-    </body>
+    </div>
+</div>  
+      
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="datatables/datatables.min.js"></script>    
+     
+    <script type="text/javascript" src="main.js"></script>  
+    
+    
+  </body>
 </html>
